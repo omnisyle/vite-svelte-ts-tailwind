b5a4a14ef9b6a9e11df752185e5c6487889d0b68
Si Le <omnisyle@gmail.com>
2021-03-20 00:36:39 -0400

    adds postcss to svelte preprocess and adds cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
+const production = process.env.NODE_ENV === 'production';
+const defaultPlugins = [
+  require("postcss-import"),
+  require("tailwindcss"),
+  require("postcss-nesting"),
+  require("autoprefixer"),
+];
+const plugins = production
+  ? [...defaultPlugins, require("cssnano")]
+  : defaultPlugins;
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("postcss-nesting"),
-    require("autoprefixer"),
-  ]
+  plugins,
 };
